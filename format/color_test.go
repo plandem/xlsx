14dd4d8eccf7a032d31d876d8da9d464dbfa8608
Andrey Gayvoronsky <plandem@gmail.com>
2018-07-21 00:16:23 -0500

    +more tests

---
+++ format/color_test.go
@@ -0,0 +1,13 @@
+package format
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestColor(t *testing.T) {
+	color := ColorToARGB("#FF00FF")
+	require.Equal(t, ARGB("FFFF00FF"), color)
+	require.Equal(t, 6, color.ToIndex())
+	require.Equal(t, -1, ColorToARGB("#112233").ToIndex())
+}
