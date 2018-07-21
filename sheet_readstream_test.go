89eb396c74d191661ab012e57627d8c3a80de0bf
Andrey Gayvoronsky <plandem@gmail.com>
2018-07-21 14:09:25 -0500

    +refactored tests for sheet content

--- sheet_readstream_test.go
+++ sheet_readstream_test.go
@@ -1,169 +1,132 @@
 package xlsx_test
 
 import (
-	"fmt"
 	"github.com/plandem/xlsx"
-	"strings"
-	"testing"
-	"github.com/stretchr/testify/require"
 	"github.com/plandem/xlsx/options"
 	"github.com/plandem/xlsx/types"
+	"github.com/stretchr/testify/require"
+	"testing"
 )
 
-//output content via normal mode
-func ExampleSheetReadStream_ReadWrite() {
-	xl, err := xlsx.Open("./test_files/example_simple.xlsx")
-	if err != nil {
-		panic(err)
+//load content using multi phases or normal mode - must be same for both
+func testSheetReadFull(t *testing.T, sheet xlsx.Sheet) {
+	cols, rows := sheet.Dimension()
+	require.Equal(t, 14, cols)
+	require.Equal(t, 28, rows)
+
+	emptyRow := []string{"", "", "", "", "", "", "", "", "", "", "", "", "", ""}
+	values := [][]string{
+		emptyRow,
+		{"    with leading space", "", "merged rows", "", "merged cols", "merged cols", "merged cols", "", "", "", "", "", "", ""},
+		{"", "", "merged rows", "", "", "", "", "", "", "", "", "", "", ""},
+		{"", "", "merged rows", "", "merged rows+cols", "merged rows+cols", "merged rows+cols", "", "", "", "", "", "", ""},
+		{"with trailing space   ", "", "merged rows", "", "merged rows+cols", "merged rows+cols", "merged rows+cols", "", "", "", "", "", "", ""},
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		{"", "", "", "1", "2", "3", "4", "5", "", "", "", "", "", ""},
+		{"", "", "", "6", "7", "8", "9", "10", "", "", "", "", "", ""},
+		{"", "", "", "11", "12", "13", "14", "15", "", "", "", "", "", ""},
+		{"", "", "", "16", "17", "18", "19", "20", "", "", "", "", "", ""},
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		{"", "", "", "", "", "", "", "", "", "", "", "", "", "last cell"},
 	}
 
-	sheet := xl.Sheet(0)
-
-	defer xl.Close()
-	defer sheet.Close()
-	fmt.Println(sheet.Dimension())
-
-	for rows := sheet.Rows(); rows.HasNext(); {
-		_, row := rows.Next()
-		fmt.Println(strings.Join(row.Values(), ","))
+	for rowIdx, rv := range values {
+		require.Equal(t, rv, sheet.Row(rowIdx).Values())
 	}
-
-/*
-Output:
-14 28
-,,,,,,,,,,,,,
-    with leading space,,merged rows,,merged cols,merged cols,merged cols,,,,,,,
-,,merged rows,,,,,,,,,,,
-,,merged rows,,merged rows+cols,merged rows+cols,merged rows+cols,,,,,,,
-with trailing space   ,,merged rows,,merged rows+cols,merged rows+cols,merged rows+cols,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,1,2,3,4,5,,,,,,
-,,,6,7,8,9,10,,,,,,
-,,,11,12,13,14,15,,,,,,
-,,,16,17,18,19,20,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,last cell
-*/
 }
 
-//output content using single phase only - it will be without merged cells info - only first cell from range will be output
-func ExampleSheetReadStream_SinglePhased() {
-	xl, err := xlsx.Open("./test_files/example_simple.xlsx")
-	if err != nil {
-		panic(err)
+//load content using single phase only - it will be without merged cells info - only first cell from range will be output
+func testSheetReadLimited(t *testing.T, sheet xlsx.Sheet) {
+	cols, rows := sheet.Dimension()
+	require.Equal(t, 14, cols)
+	require.Equal(t, 28, rows)
+
+	emptyRow := []string{"", "", "", "", "", "", "", "", "", "", "", "", "", ""}
+	values := [][]string{
+		emptyRow,
+		{"    with leading space", "", "merged rows", "", "merged cols", "", "", "", "", "", "", "", "", ""},
+		emptyRow,
+		{"", "", "", "", "merged rows+cols", "", "", "", "", "", "", "", "", ""},
+		{"with trailing space   ", "", "", "", "", "", "", "", "", "", "", "", "", ""},
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		{"", "", "", "1", "2", "3", "4", "5", "", "", "", "", "", ""},
+		{"", "", "", "6", "7", "8", "9", "10", "", "", "", "", "", ""},
+		{"", "", "", "11", "12", "13", "14", "15", "", "", "", "", "", ""},
+		{"", "", "", "16", "17", "18", "19", "20", "", "", "", "", "", ""},
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		emptyRow,
+		{"", "", "", "", "", "", "", "", "", "", "", "", "", "last cell"},
 	}
 
-	sheet := xl.SheetReader(0, false)
-
-	defer xl.Close()
-	defer sheet.Close()
-	fmt.Println(sheet.Dimension())
-
-	for rows := sheet.Rows(); rows.HasNext(); {
-		_, row := rows.Next()
-		fmt.Println(strings.Join(row.Values(), ","))
+	for rowIdx, rv := range values {
+		require.Equal(t, rv, sheet.Row(rowIdx).Values())
 	}
-
-/*
-Output:
-14 28
-,,,,,,,,,,,,,
-    with leading space,,merged rows,,merged cols,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,merged rows+cols,,,,,,,,,
-with trailing space   ,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,1,2,3,4,5,,,,,,
-,,,6,7,8,9,10,,,,,,
-,,,11,12,13,14,15,,,,,,
-,,,16,17,18,19,20,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,last cell
-*/
 }
 
-//output content using multi phases it must be same as for normal mode
-func ExampleSheetReadStream_MultiPhased() {
+func TestSpreadsheet(t *testing.T) {
 	xl, err := xlsx.Open("./test_files/example_simple.xlsx")
 	if err != nil {
 		panic(err)
 	}
 
-	sheet := xl.SheetReader(0, true)
-
 	defer xl.Close()
-	defer sheet.Close()
-	fmt.Println(sheet.Dimension())
 
-	for rows := sheet.Rows(); rows.HasNext(); {
-		_, row := rows.Next()
-		fmt.Println(strings.Join(row.Values(), ","))
+	type beforeTestFn func(t *testing.T, xl *xlsx.Spreadsheet) xlsx.Sheet
+	type testFn func(t *testing.T, sheet xlsx.Sheet)
+
+	sheetTests := []struct {
+		name     string
+		open     beforeTestFn
+		callback testFn
+	}{
+		{
+			"SheetReadStream_SinglePhased", func(t *testing.T, xl *xlsx.Spreadsheet) xlsx.Sheet { return xl.SheetReader(0, false) }, testSheetReadLimited,
+		},
+		{
+			"SheetReadStream_MultiPhased", func(t *testing.T, xl *xlsx.Spreadsheet) xlsx.Sheet { return xl.SheetReader(0, true) }, testSheetReadFull,
+		},
+		{
+			"SheetReadWrite", func(t *testing.T, xl *xlsx.Spreadsheet) xlsx.Sheet { return xl.Sheet(0) }, testSheetReadFull,
+		},
 	}
 
-/*
-Output:
-14 28
-,,,,,,,,,,,,,
-    with leading space,,merged rows,,merged cols,merged cols,merged cols,,,,,,,
-,,merged rows,,,,,,,,,,,
-,,merged rows,,merged rows+cols,merged rows+cols,merged rows+cols,,,,,,,
-with trailing space   ,,merged rows,,merged rows+cols,merged rows+cols,merged rows+cols,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,1,2,3,4,5,,,,,,
-,,,6,7,8,9,10,,,,,,
-,,,11,12,13,14,15,,,,,,
-,,,16,17,18,19,20,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,
-,,,,,,,,,,,,,last cell
-*/
+	for _, info := range sheetTests {
+		t.Run(info.name, func(tt *testing.T) {
+			sheet := info.open(tt, xl)
+			info.callback(tt, sheet)
+		})
+	}
 }
 
 func TestSheetReadStream_notImplemented(t *testing.T) {
@@ -176,16 +139,16 @@ func TestSheetReadStream_notImplemented(t *testing.T) {
 	sheet := xl.SheetReader(0, true)
 	defer sheet.Close()
 
-	require.Panics(t, func() { sheet.Col(0)})
-	require.Panics(t, func() { sheet.Cols()})
-	require.Panics(t, func() { sheet.InsertCol(0)})
-	require.Panics(t, func() { sheet.InsertRow(0)})
-	require.Panics(t, func() { sheet.DeleteRow(0)})
-	require.Panics(t, func() { sheet.DeleteCol(0)})
-	require.Panics(t, func() { sheet.SetDimension(100, 100)})
-	require.Panics(t, func() { sheet.SetActive()})
-	require.Panics(t, func() { sheet.Set(options.NewSheetOptions(options.Sheet.Visibility(types.VisibilityTypeVisible)))})
-	require.Panics(t, func() { sheet.SetName("aaa")})
+	require.Panics(t, func() { sheet.Col(0) })
+	require.Panics(t, func() { sheet.Cols() })
+	require.Panics(t, func() { sheet.InsertCol(0) })
+	require.Panics(t, func() { sheet.InsertRow(0) })
+	require.Panics(t, func() { sheet.DeleteRow(0) })
+	require.Panics(t, func() { sheet.DeleteCol(0) })
+	require.Panics(t, func() { sheet.SetDimension(100, 100) })
+	require.Panics(t, func() { sheet.SetActive() })
+	require.Panics(t, func() { sheet.Set(options.NewSheetOptions(options.Sheet.Visibility(types.VisibilityTypeVisible))) })
+	require.Panics(t, func() { sheet.SetName("aaa") })
 }
 
 func TestSheetReadStream_access(t *testing.T) {
@@ -202,5 +165,5 @@ func TestSheetReadStream_access(t *testing.T) {
 	require.Equal(t, "8", sheet.CellByRef("F11").Value())
 	require.Equal(t, "", sheet.CellByRef("F10").Value())
 	require.Equal(t, "8", sheet.Cell(5, 10).Value())
-	require.Equal(t, []string{"", "", "", "", "", "6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"}, sheet.Range("D10:H13").Values())
+	require.Equal(t, []string{"", "", "", "", "", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"}, sheet.Range("D10:H13").Values())
 }
