6f60f90e17a1af2cb3e8b9a8c72d630a4ed888f9
Andrey Gayvoronsky <plandem@gmail.com>
2018-07-29 15:17:38 -0500

    added examples for getters/setters

--- example_test.go
+++ example_test.go
@@ -558,7 +558,7 @@ func Example_copy() {
 }
 
 // Demonstrates how to get/set value for cell
-func Example_gettersAndSetters() {
+func Example_getters_and_setters() {
 	xl := xlsx.New()
 	defer xl.Close()
 
