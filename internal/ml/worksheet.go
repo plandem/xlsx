8199a1d303ba5ec3b031ca7178ce33e0c9d1ce5b
Andrey G <plandem@gmail.com>
2019-04-28 01:52:57 -0500

    Update worksheet.go
    conditional formatting hot fix

--- internal/ml/worksheet.go
+++ internal/ml/worksheet.go
@@ -25,7 +25,7 @@ type Worksheet struct {
 	CustomSheetViews      *ml.Reserved    `xml:"customSheetViews,omitempty"`
 	MergeCells            *[]*MergeCell   `xml:"mergeCells>mergeCell,omitempty"`
 	PhoneticPr            *ml.Reserved    `xml:"phoneticPr,omitempty"`
-	ConditionalFormatting *ml.Reserved    `xml:"conditionalFormatting,omitempty"`
+	ConditionalFormatting *[]*ml.Reserved `xml:"conditionalFormatting,omitempty"`
 	DataValidations       *ml.Reserved    `xml:"dataValidations,omitempty"`
 	Hyperlinks            *[]*Hyperlink   `xml:"hyperlinks>hyperlink,omitempty"`
 	PrintOptions          *ml.Reserved    `xml:"printOptions,omitempty"`
